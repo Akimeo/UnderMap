58b4487898f3e3a361e697b67b0c2b375ba571ed
Akim <aka.parinov@yandex.ru>
2019-04-30 00:03:17 +0300

    No hope

--- MAPI.py
+++ MAPI.py
@@ -5,12 +5,12 @@ import os
 from text_input import PygameTextBox, Button
 
 pygame.init()
-screen = pygame.display.set_mode((512, 412))
+screen = pygame.display.set_mode((812, 384))
 COLOR_INACTIVE = pygame.Color('lightskyblue3')
 COLOR_ACTIVE = pygame.Color('dodgerblue2')
 
 
-def make_map_file(ll, z, l, spn=None):
+def make_map_file(ll, z, l, points):
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     map_params = {
         "ll": ll,
@@ -18,8 +18,8 @@ def make_map_file(ll, z, l, spn=None):
         "z": z,
         "size": "512,384"
     }
-    if spn:
-        map_params['spn'] = spn
+    if points:
+        map_params['pt'] = points
     try:
         response = requests.get(map_api_server, params=map_params)
     except Exception:
@@ -43,12 +43,14 @@ def make_map_file(ll, z, l, spn=None):
         sys.exit(2)
 
 
-def make_spn(toponym):
-    delta_x = str(float(toponym['boundedBy']['Envelope']['upperCorner'].split()[
-        0]) - float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[0]))
-    delta_y = str(float(toponym['boundedBy']['Envelope']['upperCorner'].split()[
-        1]) - float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[1]))
-    return [delta_x, delta_y]
+def make_z(toponym):
+    delta_x = float(toponym['boundedBy']['Envelope']['upperCorner'].split()[0]) - float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[0])
+    delta_y = float(toponym['boundedBy']['Envelope']['upperCorner'].split()[1]) - float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[1])
+    z = 0
+    for i in range(17):
+        if delta_x < 360 / 2 ** (i) or delta_y < 170 / 2 ** (i):
+            z = i + 1
+    return z
 
 
 def search_place(toponym_to_find):
@@ -62,16 +64,48 @@ def search_place(toponym_to_find):
     json_response = response.json()
     toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
     address_ll = list(map(float, toponym["Point"]["pos"].split(" ")))
-    spn = make_spn(toponym)
-    return (address_ll, spn)
+    z = make_z(toponym)
+    info = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
+    if 'postal_code' in toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]:
+        post_code = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+    else:
+        post_code = None
+    return (address_ll, z, info, post_code)
 
+def click_search(pos, st_point, z):
+    x, y = pos[0] - 300, pos[1]
+    print(pos[1])
+    if x >= 0:
+        st_point_x = st_point[0] - 360 / 2 ** z
+        st_point_y = st_point[1] + 127.5 / 2 ** z
+        point_x = st_point_x + 720 / 2 ** z / 512 * x
+        point_y = st_point_y - 255 / 2 ** z / 384 * y
+        print(st_point_x, st_point_y)
+        address_ll = '{},{}'.format(point_x, point_y)
+        # address_ll = '{},{}'.format(st_point_x, st_point_y)
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {"geocode": address_ll, "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        if not response:
+            print("Ошибка выполнения запроса")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        info = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
+        if 'postal_code' in toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]:
+            post_code = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+        else:
+            post_code = None
+        return (address_ll, info, post_code)
 
-input_box = PygameTextBox(0, 0, 430, 28)
-btn = Button(600, 0, 78, 28)
+input_box = PygameTextBox(0, 0, 300, 28)
+# btn = Button(429, 0, 83, 28, input_box)
 
 starting_point = [0, 0]
 zoom = 1
 map_type = ["map"]
+points = None
 
 clock = pygame.time.Clock()
 FPS = 30
@@ -83,8 +117,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         input_box.update(events)
+        # btn.update(events)
         if input_box.is_not_active():
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    result, info, post_code = click_search(pygame.mouse.get_pos(), starting_point, zoom)
+                    if result:
+                        print(result)
+                        point = result
+                        points = point + ',flag'
+                        input_box.add_info(info)
+                        input_box.add_post_code(post_code)
+                        event_check = True
+            elif event.type == pygame.KEYDOWN:
                 event_check = True
                 if event.key == pygame.K_m:
                     if map_type[0] == "map":
@@ -111,10 +156,12 @@ while running:
                     starting_point[1] += 255 / 2 ** zoom
                     if starting_point[1] > 85:
                         starting_point[1] = 85
+                    print(starting_point[1])
                 elif event.key == pygame.K_DOWN:
                     starting_point[1] -= 255 / 2 ** zoom
                     if starting_point[1] < -85:
                         starting_point[1] = -85
+                    print(starting_point[1])
                 elif event.key == pygame.K_LEFT:
                     starting_point[0] -= 720 / 2 ** zoom
                     if starting_point[0] < -180:
@@ -125,23 +172,23 @@ while running:
                         starting_point[0] -= 360
     name = input_box.get_name()
     if name:
-        place = search_place(name)
-        make_map_file(
-            "{},{}".format(place[0][0], place[0][1]),
-            str(zoom),
-            ','.join(map_type),
-            ','.join(place[1])
-        )
-    if event_check:
+        starting_point, zoom, info, post_code = search_place(name)
+        input_box.add_info(info)
+        input_box.add_post_code(post_code)
+        points = ','.join(list(map(str, starting_point))) + ',flag'
+    if input_box.drop_check():
+        event_check = True
+        points = None
+    if name or event_check:
         make_map_file(
             "{},{}".format(starting_point[0], starting_point[1]),
             str(zoom),
-            ','.join(map_type)
-        )
+            ','.join(map_type),
+            points)
+    screen.fill((255, 255, 255))
     input_box.draw(screen)
-    btn.update(events)
-    btn.draw(screen)
-    screen.blit(pygame.image.load("map.png"), (0, 28))
+    # btn.draw(screen)
+    screen.blit(pygame.image.load("map.png"), (300, 0))
     pygame.display.flip()
     clock.tick(FPS)
     event_check = False
--- text_input.py
+++ text_input.py
@@ -1,15 +1,17 @@
 import pygame
 
 pygame.init()
+# FONT = pygame.font.Font("consola.ttf", 20)
 FONT = pygame.font.Font("UbuntuMono-R.ttf", 20)
 
 
 class Button:
-    def __init__(self, x, y, w, h, text='Искать'):
+    def __init__(self, x, y, w, h, input_box, text='Искать'):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+        self.input_box = input_box
         self.text = text
         self.rect = pygame.Rect(x, y, w, h)
         self.color_inactive = pygame.Color('gray')
@@ -21,10 +23,14 @@ class Button:
             self.color = self.color_active
         else:
             self.color = self.color_inactive
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    self.input_box.ready = True
 
     def draw(self, screen):
         text = FONT.render(self.text, 1, self.color)
-        screen.blit(text, (self.x + 5, self.y + 5))
+        screen.blit(text, (self.x + 12, self.y + 5))
         pygame.draw.rect(screen, self.color,
                          (self.x, self.y, self.w, self.h), 1)
 
@@ -37,21 +43,60 @@ class PygameTextBox:
         self.h = h
         self.text = text
         self.vis_text = text
+        self.info = ''
+        self.post_code = '-'
         self.rect = pygame.Rect(x, y, w, h)
-        self.color_inactive = pygame.Color('gray')
+        self.info_rect = pygame.Rect(x, y + 27, w, 357)
+        self.color_inactive = (160, 160, 160)
         self.color_active = (255, 204, 0)
         self.color = self.color_inactive
         self.cursor_pos = 0
         self.lb = ''
         self.rb = ''
         self.cursor_animation = 0
+        self.al_image = pygame.image.load('active_lupa.png')
+        self.il_image = pygame.image.load('inactive_lupa.png')
+        self.lupa_image = self.il_image
+        self.lupa_rect = pygame.Rect(x + 226, y, 28, 28)
+        self.ac_image = pygame.image.load('active_cross.png')
+        self.ic_image = pygame.image.load('inactive_cross.png')
+        self.cross_image = self.ic_image
+        self.cross_rect = pygame.Rect(x + 264, y, 28, 28)
+        self.ap_image = pygame.image.load('active_post.png')
+        self.ip_image = pygame.image.load('inactive_post.png')
+        self.post_image = self.ip_image
+        self.post_rect = pygame.Rect(x + 5, y + 33, 28, 28)
         self.active = False
         self.ready = False
+        self.drop = False
+        self.print_code = False
+        self.found = False
 
     def update(self, events):
+        if self.lupa_rect.collidepoint(pygame.mouse.get_pos()):
+            self.lupa_image = self.al_image
+        else:
+            self.lupa_image = self.il_image
+        if self.cross_rect.collidepoint(pygame.mouse.get_pos()):
+            self.cross_image = self.ac_image
+        else:
+            self.cross_image = self.ic_image
+        if self.post_rect.collidepoint(pygame.mouse.get_pos()):
+            self.post_image = self.ap_image
+        else:
+            self.post_image = self.ip_image
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.rect.collidepoint(event.pos):
+                if self.lupa_rect.collidepoint(event.pos):
+                    self.ready = True
+                elif self.cross_rect.collidepoint(event.pos):
+                    self.lb = self.rb = self.vis_text = self.text = self.info = ''
+                    self.cursor_pos = 0
+                    self.found = False
+                    self.drop = True
+                elif self.post_rect.collidepoint(event.pos):
+                    self.print_code = not self.print_code
+                elif self.rect.collidepoint(event.pos):
                     self.active = True
                     self.color = self.color_active
                     self.cursor_pos = min(event.pos[0] // 10,
@@ -85,7 +130,7 @@ class PygameTextBox:
                                 self.vis_text[self.cursor_pos:]
                             self.cursor_pos = max(self.cursor_pos - 1, 0)
                     elif event.key == pygame.K_DELETE:
-                        if self.rb and self.cursor_pos == 42:
+                        if self.rb and self.cursor_pos == 21:
                             self.rb = self.rb[1:]
                         else:
                             self.vis_text = self.vis_text[:self.cursor_pos] + \
@@ -97,22 +142,22 @@ class PygameTextBox:
                         if self.lb and self.cursor_pos == 0:
                             self.vis_text = self.lb[-1] + self.vis_text
                             self.lb = self.lb[:-1]
-                            if len(self.vis_text) > 42:
+                            if len(self.vis_text) > 21:
                                 self.rb = self.vis_text[-1] + self.rb
                                 self.vis_text = self.vis_text[:-1]
                         self.cursor_pos = max(self.cursor_pos - 1, 0)
                     elif event.key == pygame.K_RIGHT:
-                        if self.rb and self.cursor_pos == 42:
+                        if self.rb and self.cursor_pos == 21:
                             self.lb += self.vis_text[0]
                             self.vis_text = self.vis_text[1:] + self.rb[0]
                             self.rb = self.rb[1:]
                         self.cursor_pos = min(
-                             self.cursor_pos + 1, len(self.vis_text))
+                            self.cursor_pos + 1, len(self.vis_text))
                     else:
                         self.vis_text = self.vis_text[:self.cursor_pos] + \
                             event.unicode + self.vis_text[self.cursor_pos:]
-                        if len(self.vis_text) > 42:
-                            if self.cursor_pos == 42:
+                        if len(self.vis_text) > 21:
+                            if self.cursor_pos == 21:
                                 self.lb += self.vis_text[0]
                                 self.vis_text = self.vis_text[1:]
                             else:
@@ -123,28 +168,68 @@ class PygameTextBox:
                             self.cursor_pos += len(event.unicode)
                     self.text = self.lb + self.vis_text + self.rb
 
-
     def draw(self, screen):
         self.cursor_animation = (self.cursor_animation + 1) % 30
-        screen.fill((255, 255, 255))
         text = FONT.render(self.vis_text, 1, self.color)
         screen.blit(text, (self.x + 5, self.y + 5))
         if self.active and self.cursor_animation < 15:
             text = FONT.render("|", 1, self.color)
             screen.blit(text, (self.x + self.cursor_pos * 10, self.y + 5))
         pygame.draw.rect(screen, self.color, self.rect, 1)
+        screen.blit(self.lupa_image, (self.lupa_rect.x, self.lupa_rect.y))
+        pygame.draw.line(screen, self.color, (259, 5), (259, 23), 2)
+        screen.blit(self.cross_image, (self.cross_rect.x, self.cross_rect.y))
+        screen.blit(self.post_image, (self.post_rect.x, self.post_rect.y))
+        row = ''
+        height = 0
+        for word in self.info.split():
+            if len(row + word + ' ') > 29:
+                info_text = FONT.render(row, 1, self.color)
+                screen.blit(info_text, (self.x + 5, self.y + 63 + 20 * height))
+                row = ''
+                height += 1
+            row += word + ' '
+        if row:
+            info_text = FONT.render(row, 1, self.color)
+            screen.blit(info_text, (self.x + 5, self.y + 63 + 20 * height))
+            height += 1
+        if self.found and self.print_code:
+            pygame.draw.line(screen, self.color, (5, self.y + 66 + 20 * height), 
+                (295, self.y + 66 + 20 * height), 1)
+            post_text = FONT.render('Почтовый код: ' + self.post_code, 1, self.color)
+            screen.blit(post_text, (self.x + 5, self.y + 68 + 20 * height))
+        pygame.draw.rect(screen, self.color, self.info_rect, 1)
 
     def get_name(self):
-        if self.ready:
+        if self.ready and self.text:
             self.ready = False
+            self.found = True
+            self.active = False
+            self.color = self.color_inactive
             return self.text
+
+    def drop_check(self):
+        if self.drop:
+            self.drop = False
+            return True
+        return False
+
+    def add_info(self, info):
+        self.info = info
+
+    def add_post_code(self, post_code):
+        if post_code:
+            self.post_code = post_code
+        else:
+            self.post_code = '-'
 
     def is_not_active(self):
         return not self.active
 
+
 '''
 screen = pygame.display.set_mode((512, 412))
-input_box = PygameTextBox(0, 20, 430, 28)
+input_box = PygameTextBox(0, 0, 430 + 83, 28)
 clock = pygame.time.Clock()
 FPS = 30
 running = True
@@ -157,6 +242,7 @@ while running:
     name = input_box.get_name()
     if name:
         print(name)
+    screen.fill((255, 255, 255))
     input_box.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
